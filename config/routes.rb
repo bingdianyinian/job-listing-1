05ad22a9124d51124ae8419650d7c49ae6259210
Huang Xun <bingdianyinian@gmail.com>
2017-04-17 19:59:45 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
-    resources :jobs
-  end
-  namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
   resources :jobs do
     resources :resumes
   end
+
   root 'jobs#index'
 end
